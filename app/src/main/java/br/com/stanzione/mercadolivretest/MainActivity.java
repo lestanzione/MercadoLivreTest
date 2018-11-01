471d79b0bfb1025bc2e12b165a734d7d9dfe9034
Leandro Stanzione <stanzione.leandro@gmail.com>
2018-11-01 17:32:52 -0300

    Improve MainActivity UI

--- app/src/main/java/br/com/stanzione/mercadolivretest/MainActivity.java
+++ app/src/main/java/br/com/stanzione/mercadolivretest/MainActivity.java
@@ -2,12 +2,37 @@ package br.com.stanzione.mercadolivretest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+    @BindView(R.id.amountEditText)
+    EditText amountEditText;
+
+    @BindView(R.id.payButton)
+    Button payButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupUi();
     }
+
+    private void setupUi(){
+        ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Mercado Livre");
+    }
+
 }
