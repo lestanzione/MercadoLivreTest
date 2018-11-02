da0a798c8597981e17480e6db527c1c2721ed7cc
Leandro Stanzione <stanzione.leandro@gmail.com>
2018-11-02 14:09:26 -0300

    Add Method draft class

---
+++ app/src/main/java/br/com/stanzione/mercadolivretest/data/Method.java
@@ -0,0 +1,59 @@
+package br.com.stanzione.mercadolivretest.data;
+
+public class Method {
+
+    private String id;
+    private String name;
+    private String paymentTypeId;
+    private boolean status;
+    private String secureThumbnail;
+    private String thumbnail;
+    
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPaymentTypeId() {
+        return paymentTypeId;
+    }
+
+    public void setPaymentTypeId(String paymentTypeId) {
+        this.paymentTypeId = paymentTypeId;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public String getSecureThumbnail() {
+        return secureThumbnail;
+    }
+
+    public void setSecureThumbnail(String secureThumbnail) {
+        this.secureThumbnail = secureThumbnail;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(String thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+}
