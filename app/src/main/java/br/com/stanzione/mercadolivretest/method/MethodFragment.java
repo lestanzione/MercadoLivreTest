6b4c48e4221b9337ac7e563d78c7e6309edf2baf
Leandro Stanzione <stanzione.leandro@gmail.com>
2018-11-02 14:56:40 -0300

    Call payment_methods api method

--- app/src/main/java/br/com/stanzione/mercadolivretest/method/MethodFragment.java
+++ app/src/main/java/br/com/stanzione/mercadolivretest/method/MethodFragment.java
@@ -1,15 +1,26 @@
 package br.com.stanzione.mercadolivretest.method;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import javax.inject.Inject;
+
+import br.com.stanzione.mercadolivretest.App;
 import br.com.stanzione.mercadolivretest.R;
+import butterknife.ButterKnife;
 
 public class MethodFragment extends Fragment implements MethodContract.View{
+
+    @Inject
+    MethodContract.Presenter presenter;
+
+    private Boolean isStarted = false;
+    private Boolean isVisible = false;
 
 
     public MethodFragment() {}
@@ -18,7 +29,51 @@ public class MethodFragment extends Fragment implements MethodContract.View{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_method, container, false);
+        View view = inflater.inflate(R.layout.fragment_method, container, false);
+        //setupUi(view);
+        return view;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        setupInjector(context);
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        isStarted = true;
+        if (isVisible) {
+            presenter.getPaymentMethods();
+        }
+    }
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        isVisible = isVisibleToUser;
+        if (isVisible && isStarted) {
+            presenter.getPaymentMethods();
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        presenter.dispose();
+    }
+
+    private void setupInjector(Context context){
+        ((App) (context.getApplicationContext()))
+                .getApplicationComponent()
+                .inject(this);
+
+        presenter.attachView(this);
+    }
+
+    private void setupUi(View view) {
+        ButterKnife.bind(this, view);
     }
 
     @Override
--- app/src/main/java/br/com/stanzione/mercadolivretest/method/MethodPresenter.java
+++ app/src/main/java/br/com/stanzione/mercadolivretest/method/MethodPresenter.java
@@ -1,6 +1,13 @@
 package br.com.stanzione.mercadolivretest.method;
 
+import java.util.List;
+
+import br.com.stanzione.mercadolivretest.data.Method;
+import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
 
 public class MethodPresenter implements MethodContract.Presenter {
 
@@ -15,7 +22,24 @@ public class MethodPresenter implements MethodContract.Presenter {
 
     @Override
     public void getPaymentMethods() {
-
+        compositeDisposable.add(
+                model.fetchPaymentMethods()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<List<Method>>() {
+                               @Override
+                               public void accept(List<Method> methods) throws Exception {
+                                    System.out.println(methods);
+                                    System.out.println(methods.size());
+                               }
+                           },
+                        new Consumer<Throwable>() {
+                            @Override
+                            public void accept(Throwable throwable) throws Exception {
+                                throwable.printStackTrace();
+                            }
+                        })
+        );
     }
 
     @Override
