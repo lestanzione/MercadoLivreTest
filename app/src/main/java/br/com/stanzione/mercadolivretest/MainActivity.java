b2a5dbfad6febf7af8561b41f4fd30d0d7520095
Leandro Stanzione <stanzione.leandro@gmail.com>
2018-11-02 12:24:07 -0300

    Implement amount field transformation

--- app/src/main/java/br/com/stanzione/mercadolivretest/MainActivity.java
+++ app/src/main/java/br/com/stanzione/mercadolivretest/MainActivity.java
@@ -3,11 +3,17 @@ package br.com.stanzione.mercadolivretest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +25,11 @@ public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.payButton)
     Button payButton;
+
+    private NumberFormat nf;
+    private DecimalFormat df;
+
+    private boolean mEditing = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +44,43 @@ public class MainActivity extends AppCompatActivity {
 
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Mercado Livre");
+
+        nf = NumberFormat.getCurrencyInstance();
+        df = (DecimalFormat) nf;
+        df.applyPattern("#,##0.00");
+
+        amountEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                amountEditText.removeTextChangedListener(this);
+
+                String digits = editable.toString().replaceAll("\\D", "");
+
+                try{
+                    String formatted = df.format(Double.parseDouble(digits)/100);
+                    editable.replace(0, editable.length(), formatted);
+                } catch (NumberFormatException nfe) {
+                    editable.clear();
+                }
+
+                amountEditText.addTextChangedListener(this);
+            }
+        });
+    }
+
+    @OnClick(R.id.payButton)
+    public void payButtonClicked(){
+        NumberFormat numberFormat = NumberFormat.getCurrencyInstance();
     }
 
 }
